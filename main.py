402e45aaf7e627de3934bf0fe3002703ee3912de
Den <30498267+HazemO@users.noreply.github.com>
2022-06-15 19:01:58 +0200

    Started the code

---
+++ main.py
@@ -0,0 +1,15 @@
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+#import libs
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+#identifiy the default microphone
+with sr.Microphone() as source:
+    #listen to a command, AVD
+    audio = r.listen(source)
+
+    text = r.recognize_google(audio)
+    print(text)   
