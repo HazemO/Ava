87bd2b9b32688a229b426afd37b936c3c92edcf2
Den <30498267+HazemO@users.noreply.github.com>
2022-06-16 15:01:47 +0200

    Trying to get the audio synth to work

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 
 import speech_recognition as sr
 import pyaudio
+import os
 
 r = sr.Recognizer()
 
 #identifiy the default microphone
 with sr.Microphone() as source:
     #listen to a command, AVD
-    audio = r.listen(source)
 
-    text = r.recognize_google(audio)
-    print(text)   
+    while True:
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        print(text)   
